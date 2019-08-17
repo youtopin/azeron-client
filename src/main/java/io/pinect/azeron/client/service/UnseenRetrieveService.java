06802277729d22432443b9b456f40a8ffd421898
Sepehr <sepehrgh01@gmail.com>
2019-08-17 17:03:58 +0430

    UnseenRetrieveService has been added

---
+++ src/main/java/io/pinect/azeron/client/service/UnseenRetrieveService.java
@@ -0,0 +1,34 @@
+package io.pinect.azeron.client.service;
+
+import io.pinect.azeron.client.domain.dto.in.UnseenResponseDto;
+import io.pinect.azeron.client.service.handler.EventListener;
+import io.pinect.azeron.client.service.publisher.AzeronUnSeenQueryPublisher;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+public class UnseenRetrieveService {
+    private final AzeronUnSeenQueryPublisher azeronUnSeenQueryPublisher;
+    private final EventListenerRegistry eventListenerRegistry;
+
+    @Autowired
+    public UnseenRetrieveService(AzeronUnSeenQueryPublisher azeronUnSeenQueryPublisher, EventListenerRegistry eventListenerRegistry) {
+        this.azeronUnSeenQueryPublisher = azeronUnSeenQueryPublisher;
+        this.eventListenerRegistry = eventListenerRegistry;
+    }
+
+    public synchronized void execute(){
+        try {
+            UnseenResponseDto unseenResponseDto = azeronUnSeenQueryPublisher.publishQuery();
+            unseenResponseDto.getMessages().forEach(messageDto -> {
+                EventListener eventListener = eventListenerRegistry.getEventListenerOfChannel(messageDto.getChannelName());
+                eventListener.handle(messageDto.getObject().toString());
+            });
+        } catch (Exception e) {
+            log.error(e);
+        }
+    }
+
+}
