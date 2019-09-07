1211f6fa0860fad277feed474beacf60c8ee9e9c
Sepehr <sepehrgh01@gmail.com>
2019-09-07 13:00:45 +0430

    Issue with SingletonList has been fixed

--- src/main/java/io/pinect/azeron/client/domain/model/NatsConfigModel.java
+++ src/main/java/io/pinect/azeron/client/domain/model/NatsConfigModel.java
@@ -2,7 +2,7 @@ package io.pinect.azeron.client.domain.model;
 
 import lombok.*;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 @Getter
@@ -13,7 +13,7 @@ import java.util.List;
 @ToString
 public class NatsConfigModel {
     @Builder.Default
-    private List<String> hosts = Collections.singletonList("nats://localhost:4222");
+    private List<String> hosts = Arrays.asList("nats://localhost:4222");
     private boolean useEpoll;
     private int idleTimeOut;
     private boolean pedantic;
